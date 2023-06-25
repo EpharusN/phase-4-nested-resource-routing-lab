50db5365d1c90cf84b5ac04a9baf5696dbd8060c
Epharus Ndumia <epharusndumia@gmail.com>
2023-06-25 23:30:41 +0300

    added item routes

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,23 @@
 class ItemsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
     items = Item.all
     render json: items, include: :user
   end
+def show
+  item = Item.find(params[:id])
+  render json: item
+end
 
+def user_items
+  user = User.find(params[:user_id])
+  items = user.items
+  render json: items
+end
+private
+
+def render_not_found_response
+  render json: {error: "Item not found"}, status: :not_found
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :items, only: [:index]
-  resources :users, only: [:show]
+  resources :items, only: [:index, :create]
+  resources :users, only: [:show] do
+    get 'items', to: 'items#user_items'
+  end
+
 end
